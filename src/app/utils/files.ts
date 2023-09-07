a185691b9b476db982659c1d269b22a56952b96b
ferran.simon <ferran@nomis.es>
2023-09-07 21:39:30 +0200

    add types

--- src/app/utils/files.ts
+++ src/app/utils/files.ts
@@ -1,21 +1,18 @@
-const saveFile = async (blob) => {
-    try {
-        const handle = await window.showSaveFilePicker({
-            suggestedName: 'split.zip',
-            types: [{
-                description: 'Archivos ZIP',
-                accept: {
-                    'application/zip': ['.zip'],
-                },
-            }],
-        });
-        const writable = await handle.createWritable();
-        await writable.write(blob);
-        await writable.close();
-        return handle;
-    } catch (err) {
-        console.error(err.name, err.message);
-    }
+const saveFile = async (blob: Blob) => {
+    // @ts-ignore
+    const handle = await window.showSaveFilePicker({
+        suggestedName: 'split.zip',
+        types: [{
+            description: 'Archivos ZIP',
+            accept: {
+                'application/zip': ['.zip'],
+            },
+        }],
+    });
+    const writable = await handle.createWritable();
+    await writable.write(blob);
+    await writable.close();
+    return handle;
 }
 
 export {saveFile}
